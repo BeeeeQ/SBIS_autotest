a6ae40f9667a6764a01cddd3619a61dba0b9c7e8
si.kuricyn <si.kuricyn@tensor.ru>
2022-05-19 18:00:28 +0300

    есть авторизация

---
+++ tests/test_lesson7.py
@@ -0,0 +1,20 @@
+from atf import *
+from atf.ui import *
+from pages_inside import *
+from pages_inside.libraries.AuthControls.authForm import Adaptive
+from pages_inside.left_accordeon import LeftAccordeon
+
+class TestMilestoneCreate(TestCaseUI):
+    @classmethod
+    def setUpClass(cls):
+        cls.browser.open('https://fix-online.sbis.ru/auth/')
+        Adaptive(cls.driver).login_as('бот', 'БотА12')
+
+    def setUp(self):
+        LeftAccordeon
+
+    def test_01_create_note(self):
+        pass
+
+    def tearDown(self):
+        self.browser.close_windows_and_alert()
